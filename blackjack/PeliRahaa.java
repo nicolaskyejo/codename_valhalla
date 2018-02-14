8dd55c4b333e2371ab020d456144e6e9bbc170bf
rokkafella3 <36485974+rokkafella3@users.noreply.github.com>
2018-02-14 20:53:49 +0200

    Add files via upload

---
+++ blackjack/PeliRahaa.java
@@ -0,0 +1,21 @@
+
+package blackjack;
+
+import java.util.*;
+import javax.swing.*;
+
+/*
+ *@author Vladimir,Merja,Ville,Nicolas
+ */
+
+public class PeliRahaa {
+
+    static Scanner lukija = new Scanner (System.in);
+    
+    public static void main(String[] args)   {
+        
+        // Muutujat
+        String vastaus;
+        //  vastaus = JOptionPane.showInputDialog(" ");
+    }
+}
