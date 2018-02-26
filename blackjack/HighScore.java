5149091b4148cb282132d0381e123a028c62c5be
rokkafella3 <36485974+rokkafella3@users.noreply.github.com>
2018-02-26 11:22:22 +0200

    highscore luokka

--- blackjack/HighScore.java
+++ blackjack/HighScore.java
@@ -10,26 +10,7 @@ Tämä luoka tallenaa nimit ja highscoret
  *@author Vladimir,Merja,Ville,Nicolas
  */
 public class HighScore {
-
-  private String nimi;
-  private long score;
-  private ArrayList<Long> TopTen = new ArrayList(10);  // long koska highscore voila olla iso, ei tiedä viella
-  private ArrayList<String> TopTen_nimet = new ArrayList(10);
-
-  public void setHighScore(int index, long hscore) {  // Add highscore to top10 if highscore on hyvää
-    TopTen.add(index, hscore);
-  }
-
-  public void setHighScoreName(int index, String name) {
-    TopTen_nimet.add(index, name);
-  }
-
-  public void checkHighScore() {
-    for (int i = 10; i < 0; i--) {
-
-    }
-
-  }
+    //private String nimi;    //private long score;    private ArrayList<Long> TopTen = new ArrayList(10);  // long koska highscore voila olla iso, ei tiedä viella    private ArrayList<String> TopTen_nimet = new ArrayList(10);    /* public void setHighScore(int index, long hscore) {      TopTen.add(index, hscore);  }  public void setHighScoreName(int index, String name) {    TopTen_nimet.add(index, name);  }    */    public void checkHighScore(long hscore, String nimi) {    // Add highscore to top10 if highscore on hyvää        for (int i = 9; i < 0; i--) {            if (hscore > TopTen.get(i)) {                TopTen.add(i, hscore);                TopTen_nimet.add(i, nimi);                i = 0;            }        }    }
 
   public void getHighScore() {             //Printaa kaikki highscoret eli top10
     for (Long scores : TopTen) {
