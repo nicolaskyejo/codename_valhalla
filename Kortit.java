b5b9e240be7c90400da6225a055651befc755261
Musgo22 <36498749+Musgo22@users.noreply.github.com>
2018-02-26 10:06:28 +0200

    Add files via upload

---
+++ Kortit.java
@@ -0,0 +1,15 @@
+package blackjack;
+
+import java.util.*;
+import javax.swing.*;
+
+/*
+Tämä on missa kokeleetan kortit grafiika homat
+ */
+
+ /*
+ *@author Vladimir,Merja,Ville,Nicolas
+ */
+public class Kortit {
+
+}
---
+++ RandomCards.java
@@ -0,0 +1,15 @@
+package blackjack;
+
+import java.util.*;
+import javax.swing.*;
+
+/*
+Tämä luoka deals with randomizing cards ja korti jakaminen
+ */
+
+ /*
+ *@author Vladimir,Merja,Ville,Nicolas
+ */
+public class RandomCards {
+
+}
