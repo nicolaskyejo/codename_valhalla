02dafc47933e0cd79709c14ad5a12cb0a57f353f
Musgo22 <36498749+Musgo22@users.noreply.github.com>
2018-02-28 12:25:33 +0200

    Add files via upload

--- blackjack/Testiluokka.java
+++ blackjack/Testiluokka.java
@@ -11,6 +11,20 @@ public class Testiluokka {
         peliPakka.taysPakka();
         peliPakka.sekoita();
         
-        System.out.println(peliPakka);
+        //System.out.println(peliPakka);
+        
+        Pakka pelaaja = new Pakka();
+        
+        pelaaja.hit(peliPakka);
+        pelaaja.hit(peliPakka);
+        
+        System.out.println("Pelaajan käsi: " + pelaaja.toString());
+        
+        Pakka jakaja = new Pakka();
+        
+        jakaja.hit(peliPakka);
+        jakaja.hit(peliPakka);
+        
+        System.out.println("Jakajan käsi: " + jakaja.toString());
     }
 }
