d5eb386f915a748d105b4e45c07283da280c4343
nk <>
2018-03-01 10:10:57 +0200

    laitan Villen Pakka ja Kortti master branchille JA kortti kuvat

--- blackjack/Controller.java
+++ blackjack/Controller.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 /*
 Tämä on meijan kontrolleri mikä yhdistää kaikki luokat (MVC model)
 Tässä on main method, toisissa luokassa ei ole main methodi.
-*/
+ */
 
  /*
  *@author Vladimir,Merja,Ville,Nicolas
@@ -17,22 +17,19 @@ public class Controller {
 
     // Muutujat 
     String nimi;
-    int score;  
-    int raha=500;
-    
+    int score;
+    int raha = 500;
+
     //  Oliot
     Peli peliobjeekti = new Peli();
     HighScore nimi_ja_score = new HighScore();
 
     // Beginning
     nimi = peliobjeekti.Alus();
-    
-    // Kaikki
-    
-    
-    // End
-        nimi_ja_score.initialize();
 
+    // Kaikki
+    // End
+    nimi_ja_score.initialize();
     score = raha * 10;   // score formula
     nimi_ja_score.checkHighScore(score, nimi);  // katso onko score hyvä 
     nimi_ja_score.showHighScore();       // näytä kaikki scoret
