db7c841d3d1a6e43341c7b790791ebb1d08fa351
rokkafella3 <36485974+rokkafella3@users.noreply.github.com>
2018-03-05 10:08:39 +0200

    Add files via upload

---
+++ JOptionPaneBlackJack/HighScore.java
@@ -0,0 +1,70 @@
+package blackjack;
+
+import java.util.*;
+
+/*
+Tämä luoka tallenaa nimit ja highscoret
+ */
+
+ /*
+ *@author Vladimir,Merja,Ville,Nicolas
+ */
+public class HighScore {
+
+  private ArrayList<Integer> TopTen = new ArrayList(10);  
+  private ArrayList<String> TopTen_nimet = new ArrayList(10);
+
+  public void setHighScore(int index, int hscore) {
+    TopTen.add(index, hscore);
+  }
+
+  public void setHighScoreName(int index, String name) {
+    TopTen_nimet.add(index, name);
+  }
+
+  public int getHighScore(int index) {
+    return TopTen.get(index);
+  }
+
+  public void checkHighScore(int hscore, String nimi) {
+    for (int i = 0; i > 9; i++) {
+      if (hscore > TopTen.get(i)) {
+        TopTen.add(i, hscore);
+        TopTen_nimet.add(i, nimi);
+
+        i = 9;
+      }
+    }
+  }
+
+  /* The following fills the arrays so out of bound errors do not occur */
+  public void initialize() {
+    TopTen.add(0, 10000);
+    TopTen_nimet.add(0, "Julius Caesar");
+    TopTen.add(1, 9000);
+    TopTen_nimet.add(1, "Augustus");
+    TopTen.add(2, 8000);
+    TopTen_nimet.add(2, "Tiberius");
+    TopTen.add(3, 7000);
+    TopTen_nimet.add(3, "Caligula");
+    TopTen.add(4, 6000);
+    TopTen_nimet.add(4, "Cladius");
+    TopTen.add(5, 6000);
+    TopTen_nimet.add(5, "Nero");
+    TopTen.add(6, 6000);
+    TopTen_nimet.add(6, "Galba");
+    TopTen.add(7, 5500);
+    TopTen_nimet.add(7, "Otho");
+    TopTen.add(8, 5400);
+    TopTen_nimet.add(8, "Vitellius");
+    TopTen.add(9, 5000);
+    TopTen_nimet.add(9, "Vespasian");
+  }
+
+  public void showHighScore() {
+    for (int i = 0; i < 10; i++) {
+      System.out.println(" " + TopTen_nimet.get(i) + "\t" + TopTen.get(i));
+    }
+
+  }
+}
---
+++ JOptionPaneBlackJack/License.java
@@ -0,0 +1,15 @@
+package blackjack;
+
+
+
+/*
+ *@author Vladimir,Merja,Ville,Nicolas
+ */
+
+
+public class License {
+
+  public String License() {
+    return "This game is a first year school project done in spring 2018, the code is released under GPL v3";
+  }
+}
